9ff5529e1ca57e40f131d12ee258d92dfaaa9e2d
Jonathan Scott <jonathan.scott@gmail.com>
2019-06-26 10:18:30 -0700

    added about page breakpoint scaling, yellow logo, and fixed horizontal scroll bar in landing mobile

--- components/javascript/nav.js
+++ components/javascript/nav.js
@@ -1,1 +1,2 @@
-const navAnchors = document.querySelectorAll('nav a');
+const navAnchors = Array.from(document.querySelectorAll('nav a'));
+navAnchors.map(el => el.addEventListener('mouseover', () => el.getElementsByClassName.color = "blue"))
