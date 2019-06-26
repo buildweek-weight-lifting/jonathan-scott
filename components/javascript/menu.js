ec67128384461ad6054e4ef210a0fb31456694f2
Jonathan Scott <jonathan.scott@gmail.com>
2019-06-26 13:47:23 -0700

    added functional mobile menu with transitions

---
+++ components/javascript/menu.js
@@ -0,0 +1,14 @@
+class Menu {
+    constructor(element) {
+        this.element = element;
+        this.menuIcon = document.querySelector('.menu-icon');
+
+        this.menuIcon.addEventListener('click', () => this.menuSelect());
+    }
+    menuSelect() {
+        this.element.classList.toggle('display-menu')
+        this.element.classList.toggle('hide-menu')
+    }
+}
+const menu = document.querySelector('.mobile-menu')
+new Menu(menu);
