af0546348347ad67df044dc1f5e4fae8ebf67a3d
Jonathan Scott <jonathan.scott@gmail.com>
2019-06-27 15:29:53 -0700

    added about us animations

--- components/javascript/about.js
+++ components/javascript/about.js
@@ -5,13 +5,13 @@ class Button {
         //matches the data in the html tag to the button pushed
         this.role = this.elem.dataset.btn;
 
-        // checks to see if 'all' button is pressed. If not it returns the appropriate list of team members
+        // checks to see if 'all' button is pressed. If not it returns the appropriate list of cards
         if(this.role === 'all') {
             this.members = document.querySelectorAll('.team-member');
         } else {
             this.members = document.querySelectorAll(`.team-member[data-img="${this.role}"]`);
         }
-        this.members = Array.from(this.members).map(el => new Team(el));
+        this.members = Array.from(this.members).map(el => new Card(el));
 
         this.elem.addEventListener('click', () => this.select())
 
@@ -23,16 +23,23 @@ class Button {
         buttonElems.forEach(el => el.classList.remove('active-btn'));
 
         const team = document.querySelectorAll('.team-member');
-        team.forEach(el => el.style.display = 'none');
-
+        team.forEach(el => el.classList.add('hidden-team'));
+        team.forEach(el => el.classList.remove('team-member'));
+        
+        // setTimeout(()=> {team.forEach(el => el.classList.add('hide'))}, 2000)
+        // this.elem.classList.remove('hide')
         this.elem.classList.add('active-btn')
+        
 
-        //iterates over the list of dom elements and changes the display to flex using a method from the Team class
-        this.members.forEach(el => el.selectTeam())
+        //iterates over the list of dom elements and removes the 'hidden-team' class using a method from the Team class
+        this.members.forEach(el => el.selectTeam());
+        // if (this.elem.classList.includes('hidden-team')) {
+        //     this.elem.style.display = 'none';
+        // }
     }
 }
 
-class Team {
+class Card {
     constructor(elem) {
         this.elem = elem;
         this.img = this.elem.querySelector('img')
@@ -44,8 +51,9 @@ class Team {
     }
 
     selectTeam() {
-        // changes newly selected list from display none to flex
-        this.elem.style.display = 'flex';
+        // removes 'hidden-team' from the newly selected list
+        this.elem.classList.remove('hidden-team');
+        this.elem.classList.add('team-member');
     }
 
     hoverName() {
